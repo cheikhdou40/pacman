565bf4f49bea2b1233fb9f33ea0b805ee0010e2f
cheikhdou40 <cheikhdou40@gmail.com>
2025-01-13 23:43:40 +0100

    avant ajout fantomes

---
+++ README.md
@@ -0,0 +1,20 @@
+# Projet-Pacman
+
+Programmation C/C++
+
+## Table des Matières
+
+- [Installation](#installation)
+- [Modification du fichier cmake](#Modification)
+- [Modification du fichier framework.c](#Modification)
+- [Fonctionnalités](#Installation de certains packages)
+- [Explication de certaines fonctionnalités du main.c](#Explication)
+
+## Installation
+
+Expliquez comment installer votre projet. Par exemple :
+
+```bash
+git clone https://github.com/nom-utilisateur/nom-du-projet.git
+cd nom-du-projet
+npm install
--- src/main.c
+++ src/main.c
@@ -23,23 +23,32 @@ typedef struct {
     Coord dir;
 } Ghost;
 
+// Variable pour le score de Pac-Man
+int score = 0;
+
 // Fonction qui mange les gums pour Pac-Man
 void eat(char **level, int x, int y) {
     if (level[y][x] == '.') {
         level[y][x] = ' ';
+        score += 10; // Chaque gum rapporte 10 points
     }
+}
+
+// Fonction pour afficher le score actuel
+void displayScore() {
+    printf("Score actuel : %d\n", score);
 }
 
 // Fonction pour vérifier si le niveau est gagné
 bool win(char **level, int rows, int cols) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            if (level[i][j] == '.') {
-                return false;
-            }
+    int i = 0;
+    while (i < rows * cols) {
+        if (level[i / cols][i % cols] == '.') {
+            return false; // Il reste des gums à manger
         }
+        i++;
     }
-    return true;
+    return true; // Tous les gums ont été mangés
 }
 
 // Fonction pour vérifier si Pac-Man a perdu
@@ -50,10 +59,10 @@ bool defeat(int pacmanX, int pacmanY, Ghost *ghost) {
 // Fonction pour obtenir les directions possibles pour un fantôme
 Coord *getPotentialDirections(char **level, Ghost *ghost, int *nbDir) {
     static Coord directions[4] = {
-            {1, 0},
-            {-1, 0},
-            {0, -1},
-            {0, 1}
+            {1, 0},  // Droite
+            {-1, 0}, // Gauche
+            {0, -1}, // Haut
+            {0, 1}   // Bas
     };
 
     Coord *potentialMoves = malloc(4 * sizeof(Coord));
@@ -63,33 +72,45 @@ Coord *getPotentialDirections(char **level, Ghost *ghost, int *nbDir) {
         int newX = ghost->pos.x + directions[i].x;
         int newY = ghost->pos.y + directions[i].y;
 
+        // Vérifier que la direction est dans les limites et non bloquée
         if (newX >= 0 && newY >= 0 && newX < 28 && newY < 31 &&
             level[newY][newX] != 'H' &&
             !(directions[i].x == -ghost->dir.x && directions[i].y == -ghost->dir.y)) {
-            if (*nbDir < 4) {  // Empêche l'écriture hors limite
-                potentialMoves[(*nbDir)++] = (Coord){newX, newY};
-            }
+            potentialMoves[(*nbDir)++] = (Coord){newX, newY};
         }
     }
 
     return potentialMoves;
 }
 
+// Fonction pour déplacer le fantôme
 void ghostMove(char **level, Ghost *ghost) {
     int nbMoves;
     Coord *potentialMoves = getPotentialDirections(level, ghost, &nbMoves);
 
-    if (nbMoves > 0) {
+    if (nbMoves == 0) {
+        // Cul-de-sac : faire demi-tour
+        ghost->dir.x = -ghost->dir.x;
+        ghost->dir.y = -ghost->dir.y;
+        ghost->pos.x += ghost->dir.x;
+        ghost->pos.y += ghost->dir.y;
+    } else if (nbMoves == 1) {
+        // Une seule direction possible : avancer
+        ghost->dir.x = potentialMoves[0].x - ghost->pos.x;
+        ghost->dir.y = potentialMoves[0].y - ghost->pos.y;
+        ghost->pos = potentialMoves[0];
+    } else {
+        // Plusieurs directions : choisir aléatoirement
         int randomIndex = rand() % nbMoves;
-        Coord newPos = potentialMoves[randomIndex];
-        ghost->dir.x = newPos.x - ghost->pos.x;
-        ghost->dir.y = newPos.y - ghost->pos.y;
-        ghost->pos = newPos;
+        ghost->dir.x = potentialMoves[randomIndex].x - ghost->pos.x;
+        ghost->dir.y = potentialMoves[randomIndex].y - ghost->pos.y;
+        ghost->pos = potentialMoves[randomIndex];
     }
 
     free(potentialMoves);
 }
 
+// Variables globales pour les directions de Pac-Man
 int dirX = 0, dirY = 0;
 int nextDirX = 0, nextDirY = 0;
 
@@ -98,6 +119,7 @@ int main(int argc, char *argv[]) {
     Textures textures;
     char **level;
 
+    // Initialisation des paramètres du framework
     printf("Initialisation du framework...\n");
     init(&params, &textures, 587, 652, 5);
 
@@ -106,24 +128,27 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // Chargement des textures
     printf("Chargement des textures...\n");
     textures.texturePacman = GetTexture("pacman.bmp", &params);
     textures.textureDot = GetTexture("dot.bmp", &params);
     textures.textureWall = GetTexture("wall.bmp", &params);
     textures.textureClyde = GetTexture("clyde.bmp", &params);
 
-    if (textures.texturePacman == NULL || textures.textureDot == NULL || textures.textureWall == NULL || textures.
-            textureClyde == NULL) {
+    if (textures.texturePacman == NULL || textures.textureDot == NULL || textures.textureWall == NULL ||
+        textures.textureClyde == NULL) {
         printf("Erreur : Impossible de charger les textures : %s\n", SDL_GetError());
         return 1;
     }
 
+    // Chargement du niveau initial
     loadFirstLevel(&level);
 
     int running = 1;
-    int pacmanX = 23;
-    int pacmanY = 14;
+    int pacmanX = 23; // Position initiale de Pac-Man (x)
+    int pacmanY = 14; // Position initiale de Pac-Man (y)
 
+    // Initialisation du fantôme Clyde
     Ghost clyde = {
             .pos = {13, 14},
             .dir = {1, 0}
@@ -134,11 +159,9 @@ int main(int argc, char *argv[]) {
     while (running) {
         int input = getInput();
 
-        int newX = pacmanX;
-        int newY = pacmanY;
-
+        // Gestion des entrées utilisateur pour Pac-Man
         switch (input) {
-            case SDLK_ESCAPE:
+            case SDLK_ESCAPE: // Fermeture du jeu si Échap est pressé
                 running = 0;
                 break;
             case SDLK_UP:
@@ -159,6 +182,7 @@ int main(int argc, char *argv[]) {
                 break;
         }
 
+        // Validation de la prochaine direction
         if (pacmanY + nextDirY >= 0 && pacmanY + nextDirY < 31 &&
             pacmanX + nextDirX >= 0 && pacmanX + nextDirX < 28 &&
             level[pacmanY + nextDirY][pacmanX + nextDirX] != 'H') {
@@ -166,26 +190,42 @@ int main(int argc, char *argv[]) {
             dirY = nextDirY;
         }
 
-        newX = pacmanX + dirX;
-        newY = pacmanY + dirY;
+        // Mise à jour de la position de Pac-Man
+        int newX = pacmanX + dirX;
+        int newY = pacmanY + dirY;
 
         if (newY >= 0 && newY < 31 && newX >= 0 && newX < 28 &&
             level[newY][newX] != 'H') {
             pacmanX = newX;
             pacmanY = newY;
-            eat(level, pacmanX, pacmanY);
+            eat(level, pacmanX, pacmanY); // Pac-Man mange une pacgum
         }
 
+        // Gestion des téléportations de Pac-Man aux bords
+        if (pacmanX < 0) pacmanX = 27; // Passage du côté gauche au droit
+        if (pacmanX > 27) pacmanX = 0; // Passage du côté droit au gauche
+        if (pacmanY < 0) pacmanY = 30; // Passage du haut vers le bas
+        if (pacmanY > 30) pacmanY = 0; // Passage du bas vers le haut
+
+        // Déplacement du fantôme Clyde
         ghostMove(level, &clyde);
 
+        // Affichage du score actuel
+        displayScore();
+
+        // Vérification des conditions de victoire
         if (win(level, 31, 28)) {
             printf("Vous avez gagné !\n");
             running = 0;
-        } else if (defeat(pacmanX, pacmanY, &clyde)) {
+        }
+
+            // Vérification des conditions de défaite
+        else if (defeat(pacmanX, pacmanY, &clyde)) {
             printf("Vous avez perdu !\n");
             running = 0;
         }
 
+        // Rendu des sprites et mise à jour
         drawLevel(level, 31, 28, &params, &textures);
         drawSpriteOnGrid(textures.texturePacman, pacmanX, pacmanY, 0, &params);
         drawSpriteOnGrid(textures.textureClyde, clyde.pos.x, clyde.pos.y, 0, &params);
@@ -194,6 +234,7 @@ int main(int argc, char *argv[]) {
 
     printf("Libération des ressources...\n");
 
+    // Libération des ressources allouées pour le niveau
     for (int i = 0; i < 31; i++) {
         if (level[i] != NULL) {
             free(level[i]);
